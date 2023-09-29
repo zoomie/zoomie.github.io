403514421a87a3dd3bd76b7e0546d871c6a07ae5
Andrew Arderne <andrew.arderne@ravelin.com>
2023-09-29 15:47:31 +0100

    update payload

--- payload.js
+++ payload.js
@@ -1,4 +1,3 @@
-
 function getEmailQueryParam() {
     const params = new URLSearchParams(window.location.search);
     email = params.get('email');
@@ -28,7 +27,6 @@ function getHostname() {
     }
     return window.location.href
 }
-console.log("hostname:", getHostname());
 
 async function sendDataToEndpoint(email, device_id, host) {
     try {
@@ -54,6 +52,8 @@ async function main() {
 
     if (queryParam.includes("logout=true")) {
         displayMessage("You have been logged out");
+    } else if (queryParam.includes("popup=true")) {
+        displayPopup("It looks like you are sharing your password with a friend. Please create a new account.")
     } else {
         const email = getEmailQueryParam();
         const deviceId = await getDeviceId();
@@ -68,12 +68,45 @@ async function main() {
         document.write(htmlContent);
         sendDataToEndpoint(email, deviceId, host);
     }
-
     // setInterval(sendDataToEndpoint, 10000);
+    
 }
 
 function displayMessage(message) {
     document.write(`<h1>${message}</h1>`);
+}
+
+function displayPopup(message) {
+    const popup = document.createElement("div");
+    popup.style.position = "fixed";
+    popup.style.bottom = "0";
+    popup.style.left = "0";
+    popup.style.width = "100%";
+    popup.style.backgroundColor = "red";
+    popup.style.color = "white";
+    popup.style.padding = "10px";
+    popup.style.textAlign = "center";
+    popup.innerHTML = message;
+    document.body.appendChild(popup);
+    // exit button
+    const exitButton = document.createElement("button");
+    exitButton.innerHTML = "Exit";
+    exitButton.style.position = "absolute";
+    exitButton.style.top = "0";
+    exitButton.style.right = "0";
+    exitButton.style.padding = "10px";
+    exitButton.style.backgroundColor = "white";
+    exitButton.style.color = "red";
+    exitButton.style.border = "none";
+    exitButton.style.cursor = "pointer";
+    exitButton.style.fontSize = "16px";
+    exitButton.style.fontWeight = "bold";
+    exitButton.style.outline = "none";
+    exitButton.onclick = function() {
+        popup.style.display = "none";
+    }
+    popup.appendChild(exitButton);
+
 }
 
 main();
